a223459914cea1bcffc0ace70d66a7899846ec6b
v_dh <vincent.dhalluin@gmail.com>
2018-10-25 11:25:49 +0200

    Initial Commit

---
+++ DevrapExtensions/DevrapExtensions.h
@@ -0,0 +1,19 @@
+//
+//  DevrapExtensions.h
+//  DevrapExtensions
+//
+//  Created by vdh on 25/10/2018.
+//  Copyright © 2018 devr4p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DevrapExtensions.
+FOUNDATION_EXPORT double DevrapExtensionsVersionNumber;
+
+//! Project version string for DevrapExtensions.
+FOUNDATION_EXPORT const unsigned char DevrapExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevrapExtensions/PublicHeader.h>
+
+
